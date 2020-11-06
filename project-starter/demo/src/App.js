be185accb92fa3579078d0a3130342bf9d299d2e
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-11-06 10:22:06 -0800

    added Introduction component

--- project-starter/demo/src/App.js
+++ project-starter/demo/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Introduction from './components/Content/Introduction/Introduction';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path="/" exact component={Introduction} />
+      </Switch>
     </div>
   );
 }
---
+++ project-starter/demo/src/components/Content/Introduction/Introduction.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import classes from './Introduction.css';
+
+class Introduction extends Component {
+
+    render () {
+        return (
+            <div className={classes.Introduction}>
+                <p>Introduction here</p>
+            </div>
+        );
+    }
+}
+
+export default Introduction;
