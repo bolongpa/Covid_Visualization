453c36c308e5329cc111ac0c679182b7fd1c7a52
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-11-06 10:53:08 -0800

    added d3 map

--- project-starter/demo/src/App.js
+++ project-starter/demo/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
+import Map from './containers/Map/Map';
 import Introduction from './components/Content/Introduction/Introduction';
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path="/map" component={Map} />
         <Route path="/" exact component={Introduction} />
       </Switch>
     </div>
