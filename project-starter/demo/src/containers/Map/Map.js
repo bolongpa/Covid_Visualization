311a3369cfacedaf628d1a23b10f3c50011704cf
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-11-09 18:53:53 -0800

    add bar chart into map page

--- project-starter/demo/src/containers/Map/Map.js
+++ project-starter/demo/src/containers/Map/Map.js
@@ -1,13 +1,54 @@
-//import { format } from 'd3';
 import React, { Component } from 'react';
 import DatePicker from "react-datepicker";
-//import * as d3 from 'd3';
+
 import D3map from '../../components/Map/D3map.js';
+import BarChart from '../../components/Bar/BarChart';
+
 import "react-datepicker/dist/react-datepicker.css";
+
+// input data for bar chart
+import unemploy_data from "../../assets/data/unemployment.csv";
+import covid_data from "../../assets/data/time_series_covid19_confirmed_US.csv";
+
 class Map extends Component {
     state = {
         startDate: new Date("2020/1/22"),
-        endDate: new Date("2020/7/22")
+        endDate: new Date("2020/7/22"),
+
+        barChosenDataset: 'unemployment',
+        unemploy_data: unemploy_data,
+        covid_data: covid_data,
+
+        chartUI: {
+            bar: {
+                title: 'Bar Chart',
+                filter: 'top'
+            }
+        }
+    }
+
+    barSwitchFilterHandler = (newfilter) => {
+        this.setState({
+            chartUI: {
+                ...this.bar_state.chartUI,
+                bar: {
+                    ...this.bar_state.chartUI.bar,
+                    filter: newfilter
+                },
+            }
+        });
+    }
+
+    resetTitleHandler = (chart) => {
+        this.setState({
+            chartUI: {
+                ...this.state.chartUI,
+                [chart]: {
+                    ...this.state.chartUI[chart],
+                    title: chart.charAt(0).toUpperCase() + chart.slice(1) + " Chart Demo"
+                }
+            }
+        })
     }
 
     render() {
@@ -22,6 +63,16 @@ class Map extends Component {
                 </div>
                 <D3map startMonth={this.state.startDate} endMonth={this.state.endDate}></D3map>
                 {console.log(this.state.startDate.getMonth())}
+
+                <BarChart
+                    time={this.state.startDate}
+                    chosenDataset={this.state.barChosenDataset}
+                    unemploy_data={this.state.unemploy_data}
+                    covid_data={this.state.covid_data}
+                    title={this.state.chartUI.bar.title}
+                    filter={this.state.chartUI.bar.filter}
+                    resetTitleHandler={this.resetTitleHandler}
+                    switchFilterHandler={this.barSwitchFilterHandler} />
 
             </div>
 
