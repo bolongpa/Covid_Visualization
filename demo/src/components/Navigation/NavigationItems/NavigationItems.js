a51af2b840d2477ee5c22ca86f140557a9847abe
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-11-18 18:37:43 -0800

    renamed tabs and hid unused tabs

--- demo/src/components/Navigation/NavigationItems/NavigationItems.js
+++ demo/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
-    
+
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/">Introduction</NavigationItem>
-        <NavigationItem link="/map">Map</NavigationItem>
-        <NavigationItem link="/mapbox">Mapbox</NavigationItem>
-        <NavigationItem link="/demoPlayground">Playground</NavigationItem>
+        <NavigationItem link="/">US Labor Market Overview</NavigationItem>
+        <NavigationItem link="/map">Data Dashboard</NavigationItem>
+        {/* <NavigationItem link="/mapbox">Mapbox</NavigationItem>
+        <NavigationItem link="/demoPlayground">Playground</NavigationItem> */}
     </ul>
 );
-    
+
 export default navigationItems;
