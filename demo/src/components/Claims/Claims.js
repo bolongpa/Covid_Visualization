a3e83ec394a6bdccacfd02d0bc5ecae533803ef7
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-11-20 13:49:09 -0800

    layout of unemployment claims tab

--- demo/src/components/Claims/Claims.js
+++ demo/src/components/Claims/Claims.js
@@ -9,11 +9,17 @@ const Claims = (props) => {
 
     return (
         <div>
+            <p>Some intro of weekly claim</p>
+
+            <MapBox />
+            <p>Some conclusions of observations of the latest weekly claim (initial + continued claims) on map</p>
+
             <Donut data={NationalClaim.Jan4} />
             <Donut data={NationalClaim.Mar21} />
             <Donut data={NationalClaim.May9} />
             <Donut data={NationalClaim.Oct24} />
-            <MapBox />
+
+            <p>Conclusion of observations on map and donut chart</p>
         </div>
     )
 };
