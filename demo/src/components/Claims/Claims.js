11c12dc358f829bc95fc8df7ef39ba22c2e27a6e
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-11-20 13:33:37 -0800

    added input data for national unemployment claims

--- demo/src/components/Claims/Claims.js
+++ demo/src/components/Claims/Claims.js
@@ -3,10 +3,16 @@ import React from 'react';
 import Donut from '../DonutChart/DonutChart';
 import MapBox from '../../containers/MapBox/MapBox';
 
+import NationalClaim from '../../assets/data/nationalUnemploymentWeeklyClaims.json';
+
 const Claims = (props) => {
+
     return (
         <div>
-            <Donut />
+            <Donut data={NationalClaim.Jan4} />
+            <Donut data={NationalClaim.Mar21} />
+            <Donut data={NationalClaim.May9} />
+            <Donut data={NationalClaim.Oct24} />
             <MapBox />
         </div>
     )
